0deb3d81a95b472dbcf958e06f33a5eabede5ce7
David E. O'Brien <deo@juniper.net>
2023-06-12 20:03:16 -0700

    Ensure secrets are properly erased from memory on {Free,Open}BSD

--- pam_macros.h
+++ pam_macros.h
@@ -12,11 +12,22 @@
 #include <string.h>
 #include <stdlib.h>
 
+#if defined __FreeBSD__ || defined __OpenBSD__
+#define __BSD_VISIBLE 1
+#include <strings.h>
+/* explicit_bzero() will not be removed by a compiler's dead store optimization pass. */
+#define pwqc_overwrite_string(x) \
+do { \
+	if (x) \
+		explicit_bzero((x), strlen(x)); \
+} while (0)
+#else
 #define pwqc_overwrite_string(x) \
 do { \
 	if (x) \
 		memset((x), 0, strlen(x)); \
 } while (0)
+#endif
 
 #define pwqc_drop_mem(x) \
 do { \
--- passwdqc_memzero.c
+++ passwdqc_memzero.c
@@ -12,6 +12,9 @@ static void memzero(void *buf, size_t len)
 {
 #ifdef _MSC_VER
 	SecureZeroMemory(buf, len);
+#elif defined __FreeBSD__ || defined __OpenBSD__
+	/* explicit_bzero() will not be removed by a compiler's dead store optimization pass. */
+	explicit_bzero(buf, len);
 #else
 	memset(buf, 0, len);
 #endif
